24ed65b8d208b7ba300a318e40a9671cd2cd89b6
BASILIUS LAIA <113657819+basiliusslaia@users.noreply.github.com>
2026-02-13 15:48:03 +0700

    Update script.js

--- script.js
+++ script.js
@@ -20,6 +20,12 @@ const noMessages = [
     "Last chance!",
     "You can't catch me anyway !"
 ]
+const yesTeasePokes = [
+    "try saying no first... I bet you want to know what happens",
+    "go on, hit no... just once 👀",
+    "you're missing out",
+    "click no, I dare you"
+]
 
 
 let yesTeasedCount = 0
